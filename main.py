991874db0e57d3a4d3e863cb8e17767531bf7160
BangJhen <jhenerar21@gmail.>
2023-11-07 14:26:55 +0700

    modified main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Kalkulator sederhana
+opList = {1 : "+", 2 : "-", 3 : "/",4 : "x"}
+
 
 print("""Kalkulator Sederhana
       1. Penambahan
@@ -6,16 +8,20 @@ print("""Kalkulator Sederhana
       3. Perkalian
       4. Pembagian""")
 op = int(input("Masukkan Angka Operator Aritmatika : "))
-
+n1 = float(input("Masukkan Pembilang : "))
+n2 = float(input("Masukkan Penyebut : "))
 
 match op:
     case 1:
         n1 += n2 
+        print(f"{n1} {opList[op]} {n2} ")
     case 2:
         n1 -= n2 
+        print(f"{n1} {opList[op]} {n2} ")
     case 3:
-        n1 *= n2 
+        n1 *= n2
+        print(f"{n1} {opList[op]} {n2} ")
     case 4:
         n1 /= n2 
+        print(f"{n1} {opList[op]} {n2} ")
 
-print(n1)
