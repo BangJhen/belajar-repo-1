be4b41eefa86442a3f73557db9b355de9a1aadfe
BangJhen <jhenerar21@gmail.>
2023-11-07 14:31:35 +0700

    modified main.py

--- main.py
+++ main.py
@@ -7,18 +7,22 @@ print("""Kalkulator Sederhana
       2. Pengurangan
       3. Perkalian
       4. Pembagian""")
-op = int(input("Masukkan Angka Operator Aritmatika : "))
-n1 = float(input("Masukkan Pembilang : "))
-n2 = float(input("Masukkan Penyebut : "))
+try : 
+    op = int(input("Masukkan Angka Operator Aritmatika : "))
+    n1 = float(input("Masukkan Pembilang : "))
+    n2 = float(input("Masukkan Penyebut : "))
 
-match op:
-    case 1:
-        hasil = n1 + n2 
-    case 2:
-        hasil = n1 - n2 
-    case 3:
-        hasil = n1 * n2 
-    case 4:
-        hasil = n1 / n2 
+    match op:
+        case 1:
+            hasil = n1 + n2 
+        case 2:
+            hasil = n1 - n2 
+        case 3:
+            hasil = n1 * n2 
+        case 4:
+            hasil = n1 / n2 
 
-print(f"{n1} {opList[op]} {n2} = {hasil} ")
+    print(f"{n1} {opList[op]} {n2} = {hasil} ")
+
+except :
+    print("Masukkan dengan benar!!!")
